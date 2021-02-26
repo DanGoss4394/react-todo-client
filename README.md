4abf818c514f88da129eb5555aa8b1f903bb134c
Daniel Gosselin <dangoscode@gmail.com>
2021-02-26 15:46:03 -0700

    Add planning and Update app.js

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Todo react client
+
+
+# Components 
+
+- Todo-form
+  - form
+    - input
+    - submit btn
+
+- Todo-item 
+  - checkbox
+  - title-box
+  - font awesome icon
+
+# State
+- title or todo
+- Done? -> boolean
+- todos -> array
--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
