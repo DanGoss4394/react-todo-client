5be0f9dd8b4252775732a964c0ec52a300511367
Daniel Gosselin <dangoscode@gmail.com>
2021-02-26 15:58:15 -0700

    Add form state for title, form with input and btn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import React, { Component } from "react";
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      title: "",
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
+      <div className="app">
+        <h1>ToDo List</h1>
+        <form className="add-todo" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add ToDo"
+            onChange={this.handleChange}
+            value={this.state.title}
+          />
+          <button type="submit">Add</button>
+        </form>
       </div>
     );
   }
