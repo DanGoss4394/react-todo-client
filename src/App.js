3d1a1ef7e0bff3c2da321768fe4e2ea99ca50aad
Daniel Gosselin <dangoscode@gmail.com>
2021-03-01 12:22:03 -0700

    Update routes to heroku routes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     e.preventDefault();
     axios({
       method: "POST",
-      url: "http://localhost:5000/api/add-todo",
+      url: "https://dg-flask-todo-api.herokuapp.com/api/add-todo",
       data: {
         title: this.state.title,
         done: false,
@@ -41,7 +41,7 @@ class App extends Component {
   handleDelete = (id) => {
     axios({
       method: "DELETE",
-      url: `http://localhost:5000/api/delete-todo/${id}`,
+      url: `https://dg-flask-todo-api.herokuapp.com/api/delete-todo/${id}`,
     })
       .then((res) => {
         this.setState({
@@ -67,7 +67,7 @@ class App extends Component {
   componentDidMount() {
     axios({
       method: "GET",
-      url: "http://localhost:5000/api/get-all-todos",
+      url: "https://dg-flask-todo-api.herokuapp.com/api/get-all-todos",
     })
       .then((res) => {
         this.setState({
